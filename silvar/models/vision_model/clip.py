7f8e874bfe3a0bcf98d0da3436493f12761eafd6
Hanh <hanhpt.phamtan@gmail.com>
2025-01-31 18:20:49 -0500

    adding deepseek

--- silvar/models/vision_model/clip.py
+++ silvar/models/vision_model/clip.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class CLIP(nn.Module):
     def __init__(self):
         super(CLIP, self).__init__()
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").vision_model
+        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16").vision_model
         self.num_features = 768
 
     def forward(self, x):
